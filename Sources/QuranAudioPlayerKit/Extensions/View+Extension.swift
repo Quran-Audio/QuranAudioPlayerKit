a8e8cfe0f9817c9390e379b2fb0cb6c0c078bdb4
Mohammed Shafeer <shafeer@digivalsolutions>
2022-02-18 08:15:06 +0530

    All praise is to ALLAH - fixed navigation style

--- Sources/QuranAudioPlayerKit/Extensions/View+Extension.swift
+++ Sources/QuranAudioPlayerKit/Extensions/View+Extension.swift
@@ -9,10 +9,10 @@ import SwiftUI
 
 extension View {
     public func deviceNavigationViewStyle() -> AnyView {
-        if TargetDevice.currentDevice == .iPad {
-            return AnyView(self.navigationViewStyle(StackNavigationViewStyle()))
-        } else {
+        if TargetDevice.currentDevice == .nativeMac {
             return AnyView(self.navigationViewStyle(DefaultNavigationViewStyle()))
+        } else {
+            return AnyView(self.navigationViewStyle(StackNavigationViewStyle()))
         }
     }
 }
